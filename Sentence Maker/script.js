52b3880e75a01943a9129892eddb8f94aa9d7a2b
Sahil Singh <akasahilsingh@gmail.com>
2025-07-14 20:55:08 +0530

    Sentence Maker

---
+++ Sentence Maker/script.js
@@ -0,0 +1,22 @@
+
+let adjective = "beutiful";
+let noun = "girl";
+let verb = "air";
+let place = "beutiful garden";
+let adjective2 = "nose ring";
+let noun2 = "Momos";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective +" "+ noun + " who loved to eat " + noun2 + ". The "  + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "beutiful";
+ noun = "women";
+ verb = "Cold";
+ place = "Mountains";
+adjective2 = "ear ring";
+ noun2 = "Chicken";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The "  + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
