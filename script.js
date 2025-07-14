8d856937a65d6500819bb53b3e57e5cf3a58e57a
Sahil Singh <akasahilsingh@gmail.com>
2025-07-14 20:52:15 +0530

    JS Basics Jarvis

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Jarvis";
+let botLocation = "New York";
+let favoriteLanguage = "Python";
+
+console.log("My name is " + botName + 
+" and I live on " + botLocation + ".")
+
+console.log("My favorite programming language is " + favoriteLanguage + ".")
+
+let codingFact = "Its easy and can create various automation in " +  favoriteLanguage;
+
+console.log(codingFact);
+
+codingFact = "Its also used in Data scince and Machine language too " + favoriteLanguage;
+
+console.log(codingFact);
+
+codingFact = "Its fun to work in " + favoriteLanguage;
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
